69c75d46792c65db19d86e63a19c44c26ff8cadc
MikhailKrakhin <staisi.kramer@gmail.com>
2022-09-19 03:03:46 +0300

    DZ0

---
+++ DZ0/Program.cs
@@ -0,0 +1,22 @@
+﻿// /*
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// */
+
+
+ Console.WriteLine ("Введите первое число: ");
+ int a = int.Parse (Console.ReadLine());
+ Console.WriteLine ("Введите второе число: ");
+ int b = int.Parse (Console.ReadLine());
+
+bool Chto0 = a > b;
+bool Chto1 = a < b;
+
+if (Chto0)
+{
+    Console.WriteLine ($"Число {a} большее, число {b} меньшее. ");
+}
+ else
+ {
+    Console.WriteLine ($"Число {a} большее, число {b} меньшее. ");
+ }
+ 
